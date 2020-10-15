c1aff960331d4f53a795bcc5e98ec5e71978c812
aripo.ir <5645612-aripoco@users.noreply.gitlab.com>
2020-10-15 18:18:21 +0330

    Make many function

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ print('Hello Git')
 def get_full_name(fname, lname):
   return "%s %s" %(fname, lname)
 
+get_full_name('Amin', 'Pourzare')
 
 def calc_age(birth_of_date):
   return now() - birth_of_date
