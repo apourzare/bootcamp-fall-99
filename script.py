9953726566599e7f842f53100ba6c02794b2c641
aripo.ir <5645612-aripoco@users.noreply.gitlab.com>
2020-10-15 18:29:31 +0330

    Make function for connect to database

--- script.py
+++ script.py
@@ -11,3 +11,9 @@ get_full_name('Amin', 'Pourzare')
 
 def calc_age(birth_of_date):
   return now() - birth_of_date
+
+def connect_to_db(host, db_name, username, password):
+  '''
+  Connect to database 
+  '''
+  return True
