9dc04397f8a27e1801e4f108449f2e3573afdb15
aripo.ir <5645612-aripoco@users.noreply.gitlab.com>
2020-10-27 18:23:34 +0330

    Meet with getter, setter and deleter

--- script.py
+++ script.py
@@ -52,25 +52,63 @@
 
 
 class Student:
+
   def __init__(self, fname, lname, faname, n_code, age, sid, majore, e_year):
-    self.fname = fname
-    self.lname = lname
-    self.faname = faname
-    self.n_code = n_code
-    self.age = age
-    self.sid = sid
-    self.majore = majore
-    self.e_year = e_year
+    self.__fname = fname
+    self.__lname = lname
+    self.__faname = faname
+    self.__n_code = n_code
+    self.__age = age
+    self.__sid = sid
+    self.__majore = majore
+    self.__e_year = e_year
+
+  def get_fname(self):
+    return self.__fname
+
+  def get_lname(self):
+    return self.__lname
+
+  def get_faname(self):
+    return self.__faname
   
-  def get_full_name(self):
-    return "%s %s" %(self.fname, self.lname)
+  def get_n_code(self):
+    return self.__n_code
 
   def get_age(self):
-    return self.age
+    return self.__age
+
+  def set_age(self, age):
+    self.__age = age
+
+  def del_age(self):
+    self.__age = None
+    # del self.__age
+
+  def get_sid(self):
+    return self.__sid
+
+  def get_majore(self):
+    return self.__majore
+
+  def get_e_year(self):
+    return self.__e_year
+
 
 
 s1 = Student('Amin', 'Pourzare', 'Hossein', '1234', 31, 99141625, 'Computer', 1399)
 
 
-print(s1.get_full_name())
+# print(s1.get_full_name())
+# print(s1.get_age())
+
+# print(s1.get_fname())
+# print(s1.get_age())
+# s1.set_age(32)
 print(s1.get_age())
+s1.del_age()
+print(s1.get_age())
+s1.set_age(32)
+print(s1.get_age())
+# del s1
+# print(s1.get_name())
